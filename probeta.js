e7f52e2409c5b6f6bd3b09834dde06e1fcc497a8
Francisco Maseda <frmadem@gmail.com>
2018-09-24 13:50:50 +0200

    Agregado bin de probeta

---
+++ probeta.js
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+const {escritor} = require("./index.js");
+
+const {ArgumentParser} = require("argparse");
+
+const fs = require("fs");
+
+const parser = new ArgumentParser({
+
+	version: '0.0.2',
+
+	addHelp: true,
+
+	description: "Sistema de apoyo"
+
+})
+
+const subparser = parser.addSubparsers({
+
+	title: "subcomandos",
+
+	dest: "cmd"
+
+})
+
+const proceso = subparser.addParser("proceso", {
+
+	addHelp: true,
+
+	description: "Agrega un proceso al proyecto",
+
+	help: "Permite agregar un proceso al proyecto"
+})
+
+
+proceso.addArgument(
+
+	["-n", "--nombre"],
+
+	{
+		action: "store",
+	
+		addHelp: true,
+
+		help: "Establece el nombre del proceso a agregar",
+
+		required: true
+	}
+
+)
+
+proceso.addArgument(
+
+	"-t",
+
+	{
+		action: "storeConst",
+
+		constant: "con-probeta", 
+
+		addHelp: true,
+
+		help: "Agrega una unidad de test (probeta) al proceso"
+	}
+
+)
+
+
+const args = parser.parseArgs();
+
+
+const acciones = {
+
+	proceso: function(args){
+
+		if(!args.nombre)
+			error(`[proceso]: Falta el nombre del proceso a agregar`)
+
+		if(!args.nombre.match(/^([A-Z1-9])(\w+)\.(\w+)$/)){
+			error(`[proceso]: El nombre del proceso no es correcto: [Familia.nombre]`)
+		}
+
+		if(!process.env["PROBETA_PROYECTO_HOME"])		
+			error(`[proceso]: la variable de entorno 'PROBETA_PROYECTO_HOME' no está definida`)
+
+		try{
+
+			escritor.escribirProceso({
+
+				proceso: args.nombre,
+
+				RUTA: process.env["PROBETA_PROYECTO_HOME"] + "/lib/procesos"
+
+			})
+
+			console.log(args)
+
+			if(args.t === "con-probeta"){
+
+				acciones.proceso_probeta(args);
+
+			}
+
+		}
+		catch(err){
+			error(`[proceso]: se ha producido un error ${err}`)
+		}
+
+	},
+
+	proceso_probeta: function(args){
+
+		if(!args.nombre)
+			error(`[proceso-probeta]: Falta el nombre del proceso de la probeta a agregar`)
+
+		if(!args.nombre.match(/^([A-Z1-9])(\w+)\.(\w+)$/)){
+			error(`[proceso-probeta]: El nombre del proceso no es correcto: [Familia.nombre]`)
+		}
+
+		if(!process.env["PROBETA_PROYECTO_HOME"])		
+			error(`[proceso-probeta]: la variable de entorno 'PROBETA_PROYECTO_HOME' no está definida`)
+
+		if(!fs.existsSync(process.env["PROBETA_PROYECTO_HOME"] + "/test")){
+
+			error(`[proceso-probeta]: no existe fichero de test`)
+		}
+
+		if(!fs.existsSync(process.env["PROBETA_PROYECTO_HOME"] + "/test/procesos")){
+
+			error(`[proceso-probeta]: no existe fichero de test/procesos`)
+		}
+
+		try{
+
+			escritor.escribirProcesoProbeta({
+
+				proceso: args.nombre,
+
+				RUTA: process.env["PROBETA_PROYECTO_HOME"] + "/test/procesos"
+
+			})
+		}
+		catch(err){
+
+			error(`[proceso]: se ha producido un error ${err}`)
+
+		}
+
+	}
+
+
+}
+
+acciones[args.cmd](args);
+
+function error(err){
+
+	console.log("\x1b[31m", err);
+	console.log("\x1b[0m", "");
+
+	process.exit(1);
+	
+
+}
+
+
